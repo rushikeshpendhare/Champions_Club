06c6a47498125068375833c5ea864699fbf855e0
rushikeshpendhare <rishikeshpendhare@gmail.com>
2024-07-23 05:30:35 -0400

    Initial commit

---
+++ slider fol garb/script.js
@@ -0,0 +1,11 @@
+{/* <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+<script>
+    var swiper = new Swiper('.swiper-container', {
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+</script> */}
+
